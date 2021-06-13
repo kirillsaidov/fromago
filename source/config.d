2626fb80506f226715c8b1d1df7695debb3a9f5b
rillki <rill.ki@yahoo.com>
2021-06-13 07:50:50 +0600

    finished working on language settings

--- source/config.d
+++ source/config.d
@@ -4,7 +4,7 @@ module config;
 import std.json: JSONValue, parseJSON;
 import std.string: strip;
 import std.stdio: File, readln;
-import std.file: exists, mkdir;
+import std.file: exists, mkdirRecurse, getcwd;
 import std.path: buildPath;
 import std.conv: to;
 
@@ -28,10 +28,10 @@ public {
 	// sets up a config directory for the application
 	void configSetup() {
 		// check if config directory exists
-		if(!configdir.exists) {
-			configdir.mkdir;
+		if(!getConfigPath.exists) {
+			getConfigPath.mkdirRecurse;
 		}
-		
+				
 		// get language code (load language config file)
 		languageCode = loadLangConfig();
 		
@@ -52,22 +52,23 @@ public {
 
 private {
 	// config directories and files
-	immutable string configdir = ".config";
-	immutable string configlang = "lang.config";
-	immutable string path_configlang = buildPath(configdir, configlang);
+	immutable string configDir = "Users".buildPath("Shared").buildPath("Fromago");
+	immutable string configFileLang = "lang.config";
 
 	// returns the code of the language (it looks for language.config file in )
 	LangCode loadLangConfig() {	
 		// check if file exists, if not, create the file; get the lang code
 		LangCode langCode = LangCode.English;
-		if(!path_configlang.exists) {
-			auto file = File(path_configlang, "w");
-			file.writeln(LangCode.English);
-			file.close();
+		if(!getConfigPath.buildPath(configFileLang).exists) {
+			auto file = File(getConfigPath.buildPath(configFileLang), "w");
+			scope(exit) { file.close(); }
+			
+			file.writeln(langCode);
 		} else {
-			auto file = File(path_configlang, "r");
-			langCode = file.readln().strip().to!LangCode;
-			file.close();
+			auto file = File(getConfigPath.buildPath(configFileLang), "r");
+			scope(exit) { file.close(); }
+			
+			langCode = file.readln.strip.to!LangCode;
 		}
 	
 		return langCode;
@@ -76,9 +77,15 @@ private {
 	// save file config
 	void saveLangConfig(LangCode langCode) {		
 		// save the config
-		auto file = File(path_configlang, "w");
+		auto file = File(getConfigPath.buildPath(configFileLang), "w");
+		scope(exit) { file.close(); }
+		
 		file.writeln(langCode);
-		file.close();
+	}
+	
+	// returns path to fromago config directory
+	string getConfigPath() {
+		return getcwd.buildPath(configDir);
 	}
 }
 
