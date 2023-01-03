02099527ab8660933c38ebe9bdd492b853deaeed
Kirill Saidov <kirillsaidovd@yahoo.com>
2023-01-03 11:10:04 +0600

    calculating saltring time formula

--- source/menu/submenu/salting.time.d
+++ source/menu/submenu/salting.time.d
@@ -4,183 +4,221 @@ module menu.submenu.salting.time;
 import meta;
 
 void calculateSaltingTime() {
-	// calculateSaltingTime window
-	new DialogWindow(mainWindow, 
-		lang["Salting Time Calc"].str, 
-		[lang["Calculate"].str, lang["Clear"].str, lang["Close"].str], 
-		[ResponseType.YES, ResponseType.REJECT, ResponseType.CANCEL],
-		new CalculatorSaltingTime()
-	);
+    // calculateSaltingTime window
+    new DialogWindow(mainWindow, 
+        lang["Salting Time Calc"].str, 
+        [lang["Calculate"].str, lang["Clear"].str, lang["Close"].str], 
+        [ResponseType.YES, ResponseType.REJECT, ResponseType.CANCEL],
+        new CalculatorSaltingTime()
+    );
 }
 
 
 // solution concentration calculator
 class CalculatorSaltingTime : DialogForm {
-	import std.conv: to;
-	
-	// variables
-	private {
-		// 'what' labels
-		PadLabel labelConcentration;
-		PadLabel labelRequiredTime;
-		PadLabel labelCheeseTotalWeight;
-		PadLabel labelTotalSaltingTime;
-		
-		// unit measure labels
-		PadLabel labelConcentrationUnit;
-		PadLabel labelRequiredTimeUnit;
-		PadLabel labelCheeseTotalWeightUnit;
-		PadLabel labelTotalSaltingTimeUnit;
+    import std.conv: to;
+    import std.array: split;
+    import core.time: minutes;
+    import std.datetime.systime : SysTime, Clock;
+    
+    // variables
+    private {
+        // 'what' labels
+        PadLabel labelConcentration;
+        PadLabel labelRequiredTime;
+        PadLabel labelCheeseTotalWeight;
+        PadLabel labelTotalSaltingTime;
+        PadLabel labelEndSaltingTimeFromNow;
+        
+        // unit measure labels
+        PadLabel labelConcentrationUnit;
+        PadLabel labelRequiredTimeUnit;
+        PadLabel labelCheeseTotalWeightUnit;
+        PadLabel labelTotalSaltingTimeUnit;
 
-		// entry text box
-		PadEntry entryConcentration;
-		PadEntry entryRequiredTime;
-		PadEntry entryCheeseTotalWeight;
-		PadEntry entryTotalSaltingTime;
-		
-		// concentration-time coefficient
-		private float coef = 0;
-	}
-	
-	
-	//constructor
-	this(int borderWidth = 7, int marginBottom = 7) {
-		// calling parent's constructor
-		super();
-		
-		// border width
-		setBorderWidth(borderWidth);
-		
-		// column 1
-		labelConcentration = new PadLabel(BoxJustify.Left, lang["Solution concentration"].str);
-		attach(labelConcentration, 0, 0, 1, 1);
+        // entry text box
+        PadEntry entryConcentration;
+        PadEntry entryRequiredTime;
+        PadEntry entryCheeseTotalWeight;
+        PadEntry entryTotalSaltingTime;
+        PadEntry entryEndSaltingTimeFromNow;
+    }
+    
+    
+    //constructor
+    this(int borderWidth = 7, int marginBottom = 7) {
+        // calling parent's constructor
+        super();
+        
+        // border width
+        setBorderWidth(borderWidth);
+        
+        // column 1
+        labelConcentration = new PadLabel(BoxJustify.Left, lang["Solution concentration"].str);
+        attach(labelConcentration, 0, 0, 1, 1);
 
-		labelRequiredTime = new PadLabel(BoxJustify.Left, lang["Required time"].str);
-		attach(labelRequiredTime, 0, 1, 1, 1);
+        labelRequiredTime = new PadLabel(BoxJustify.Left, lang["Required time"].str);
+        attach(labelRequiredTime, 0, 1, 1, 1);
 
-		labelCheeseTotalWeight = new PadLabel(BoxJustify.Left, lang["Cheese total weight"].str);
-		attach(labelCheeseTotalWeight, 0, 2, 1, 1);
-		
-		labelTotalSaltingTime = new PadLabel(BoxJustify.Left, lang["Total salting time"].str);
-		attach(labelTotalSaltingTime, 0, 3, 1, 1);
-		
-		// column 2
-		entryConcentration = new PadEntry(BoxJustify.Left, "0");
-		attach(entryConcentration, 1, 0, 1, 1);
+        labelCheeseTotalWeight = new PadLabel(BoxJustify.Left, lang["Cheese total weight"].str);
+        attach(labelCheeseTotalWeight, 0, 2, 1, 1);
+        
+        labelTotalSaltingTime = new PadLabel(BoxJustify.Left, lang["Total salting time"].str);
+        attach(labelTotalSaltingTime, 0, 3, 1, 1);
 
-		entryRequiredTime = new PadEntry(BoxJustify.Left, "0");
-		attach(entryRequiredTime, 1, 1, 1, 1);
+        labelEndSaltingTimeFromNow = new PadLabel(BoxJustify.Left, lang["End salting time"].str);
+        attach(labelEndSaltingTimeFromNow, 0, 4, 1, 1);
+        
+        // column 2
+        entryConcentration = new PadEntry(BoxJustify.Left, "0");
+        attach(entryConcentration, 1, 0, 1, 1);
 
-		entryCheeseTotalWeight = new PadEntry(BoxJustify.Left, "0");
-		attach(entryCheeseTotalWeight, 1, 2, 1, 1);
-		
-		entryTotalSaltingTime = new PadEntry(BoxJustify.Left, "0");
-		attach(entryTotalSaltingTime, 1, 3, 1, 1);
-				
-		// column 3
-		labelConcentrationUnit = new PadLabel(BoxJustify.Left, "%");
-		attach(labelConcentrationUnit, 2, 0, 1, 1);
+        entryRequiredTime = new PadEntry(BoxJustify.Left, "0");
+        attach(entryRequiredTime, 1, 1, 1, 1);
 
-		labelRequiredTimeUnit = new PadLabel(BoxJustify.Left, lang["mins per 100 grams"].str);
-		attach(labelRequiredTimeUnit, 2, 1, 1, 1);
+        entryCheeseTotalWeight = new PadEntry(BoxJustify.Left, "0");
+        attach(entryCheeseTotalWeight, 1, 2, 1, 1);
+        
+        entryTotalSaltingTime = new PadEntry(BoxJustify.Left, "0");
+        attach(entryTotalSaltingTime, 1, 3, 1, 1);
 
-		labelCheeseTotalWeightUnit = new PadLabel(BoxJustify.Left, lang["grams"].str);
-		attach(labelCheeseTotalWeightUnit, 2, 2, 1, 1);
-		
-		labelTotalSaltingTimeUnit = new PadLabel(BoxJustify.Left, lang["h-hours, m-minutes"].str);
-		attach(labelTotalSaltingTimeUnit, 2, 3, 1, 1);
-		
-		// set margin space
-		setMarginBottom(marginBottom);
-	}
-	
-	// do something on action
-	void onAction(int r, Dialog d) {
-		switch(r) with(ResponseType) {
-			case YES:
-				{
-					// get all data from entries
-					auto data = getData();
-					
-					// if concentration is 0, do nothing
-					if(!data["%"]) {
-						return;
-					}
-					
-					// calculations
-					auto temp_coef = calcCoefficient(data["time"], data["%"]);
-					if(data["%"] && data["time"] && data["weight"]) {
-						// calculating salting time
-						auto temp_saltingTime = calcTotalSaltingTimeReadable(data["weight"], data["%"], temp_coef);
-						
-						entryTotalSaltingTime.setText(temp_saltingTime);
-					} else if(data["%"] && !data["time"]) {
-						// calculating required time
-						auto temp_requiredTime = calcRequiredTime(data["%"], coef);
-						
-						entryRequiredTime.setText(temp_requiredTime);
-					}
-					
-					coef = ((temp_coef == 0) ? (coef) : (temp_coef));
-				}
-				break;
-			case REJECT:
-				clearEntries();
-				break;
-			case CANCEL:
-				d.destroy();
-				break;
-			default:
-				break;
-		}
-	}
-	
-	// get grid instance
-	Grid getInstance() {
-		return this;
-	}
-	
-	// sets everything to zero
-	private void clearEntries() {
-		entryConcentration.setText("0");
-		entryTotalSaltingTime.setText("0");
-		entryCheeseTotalWeight.setText("0");
-		entryRequiredTime.setText("0");
-	}
-	
-	// get all data
-	private float[string] getData() {
-		return to!(float[string])([
-			"%" : entryConcentration.getText().replaceEmptyWith("0").strTo!float(),
-			"time" : entryRequiredTime.getText().replaceEmptyWith("0").strTo!float(),
-			"weight" : entryCheeseTotalWeight.getText().replaceEmptyWith("0").strTo!float(),
-			"saltingTime" : 0
-		]);
-	}
-	
-	// calculating concentration-time coefficient
-	private float calcCoefficient(float time, float concentration) {
-		return (time / concentration);
-	}
-	
-	// calculating salting time (readable human-friendly format)
-	private string calcTotalSaltingTimeReadable(float weight, float concentration, float coef) {
-		float salting_time = weight / 100 * concentration * coef / 60;
-		int hours = to!int(salting_time);
-		int minutes = to!int((salting_time - hours) * 60);
-		
-		return (to!string(hours) ~ "h " ~ to!string(minutes) ~ "m");
-	}
-	
-	// calculating salting time
-	private string calcTotalSaltingTime(float weight, float concentration, float coef) {
-		return (to!string(weight / 100 * concentration * coef / 60));
-	}
-	
-	// calculating new required time
-	private string calcRequiredTime(float concentration, float coef) {
-		return (to!string(concentration * coef));
-	}
+        entryEndSaltingTimeFromNow = new PadEntry(BoxJustify.Left, Clock.currTime.toISOExtString().split(".")[0]);
+        attach(entryEndSaltingTimeFromNow, 1, 4, 1, 1);
+                
+        // column 3
+        labelConcentrationUnit = new PadLabel(BoxJustify.Left, "%");
+        attach(labelConcentrationUnit, 2, 0, 1, 1);
+
+        labelRequiredTimeUnit = new PadLabel(BoxJustify.Left, lang["mins per 100 grams"].str);
+        attach(labelRequiredTimeUnit, 2, 1, 1, 1);
+
+        labelCheeseTotalWeightUnit = new PadLabel(BoxJustify.Left, lang["grams"].str);
+        attach(labelCheeseTotalWeightUnit, 2, 2, 1, 1);
+        
+        labelTotalSaltingTimeUnit = new PadLabel(BoxJustify.Left, lang["h-hours, m-minutes"].str);
+        attach(labelTotalSaltingTimeUnit, 2, 3, 1, 1);
+        
+        // set margin space
+        setMarginBottom(marginBottom);
+    }
+    
+    // do something on action
+    void onAction(int r, Dialog d) {
+        switch(r) with(ResponseType) {
+            case YES:
+                {
+                    // get all data from entries
+                    auto data = getData();
+                    
+                    // if concentration is 0, do nothing
+                    if(!data["%"] || data["%"] == 0) {
+                        return;
+                    }
+                    
+                    // // calculations
+                    // auto temp_coef = calcCoefficient(data["time"], data["%"]);
+                    // if(data["%"] && data["time"] && data["weight"]) {
+                    //     // calculating salting time
+                    //     auto temp_saltingTime = calcTotalSaltingTimeReadable(data["weight"], data["%"], temp_coef);
+                        
+                    //     entryTotalSaltingTime.setText(temp_saltingTime);
+                    // } else if(data["%"] && !data["time"]) {
+                    //     // calculating required time
+                    //     auto temp_requiredTime = calcRequiredTime(data["%"], coef);
+                        
+                    //     // update required time
+                    //     entryRequiredTime.setText(temp_requiredTime);
+
+                    //     // updat end salting time
+                    //     // endSaltingTime = Clock.currTime();
+                    //     // endSaltingTime += 
+                    // }
+                    
+                    // coef = ((temp_coef == 0) ? (coef) : (temp_coef));
+
+                    // calculations
+                    if(data["%"] && data["time"] && data["weight"]) {
+                        // 0. calculate salting time coefficient
+                        immutable coef = calcCoefficient(data["time"], data["%"]);
+
+                        // 1. calculate required salting time amount
+                        immutable rstAmount = calcRequiredSaltingTimeAmount(data["%"], coef);
+
+                        // 2. convert to readable format
+                        immutable rstAmountReadable = convertRequiredSaltingTimeToReadable(rstAmount);
+
+                        // 3. calculate end salting time
+                        immutable endSaltingTimeReadable = calcEndSaltingTimeReadable(rstAmount);
+
+                        // 4. update salting time entry with end salting time label
+                        entryTotalSaltingTime.setText(rstAmountReadable);
+                        entryEndSaltingTimeFromNow.setText(endSaltingTimeReadable);
+                    }
+                }
+                break;
+            case REJECT:
+                clearEntries();
+                break;
+            case CANCEL:
+                d.destroy();
+                break;
+            default:
+                break;
+        }
+    }
+    
+    // get grid instance
+    Grid getInstance() {
+        return this;
+    }
+    
+    // sets everything to zero
+    private void clearEntries() {
+        entryConcentration.setText("0");
+        entryTotalSaltingTime.setText("0");
+        entryCheeseTotalWeight.setText("0");
+        entryRequiredTime.setText("0");
+        entryEndSaltingTimeFromNow.setText(Clock.currTime.toISOExtString());
+    }
+    
+    // get all data
+    private float[string] getData() {
+        return to!(float[string])([
+            "%" : entryConcentration.getText().replaceEmptyWith("0").strTo!float(),
+            "time" : entryRequiredTime.getText().replaceEmptyWith("0").strTo!float(),
+            "weight" : entryCheeseTotalWeight.getText().replaceEmptyWith("0").strTo!float(),
+            "saltingTime" : 0
+        ]);
+    }
+
+    /// calculating required salting time amount
+    private ulong calcRequiredSaltingTimeAmount(in float concentration, in float weight) {
+        return cast(ulong)(weight / 100 * concentration / 60);
+    }
+
+    /// calculates end salting time and converts it to a readable human-friendly format
+    private string calcEndSaltingTimeReadable(in ulong rstAmount) {
+        SysTime currentTime = Clock.currTime();
+        currentTime += rstAmount.minutes();
+        return currentTime.toISOExtString().split(".")[0];
+    }
+    
+    /// convert salting time amount to a readable human-friendly format
+    private string convertRequiredSaltingTimeToReadable(in ulong rstAmount) {
+        import std;
+        immutable rstPeriod = rstAmount.to!float / 60.0;
+        writeln(rstAmount);
+        writeln(rstPeriod);
+        immutable hours = rstPeriod.to!ulong;
+        immutable minutes = to!ulong((rstPeriod - hours) * 60);
+        
+        return (to!string(hours) ~ "h " ~ to!string(minutes) ~ "m");
+    }
+
+    // calculate salting time amount
+    private ulong calcTotalSaltingTime(float weight, float concentration, float coef) {
+        return cast(ulong)(weight / 100 * concentration * coef / 60);
+    }
 }
 
 
--- source/meta.d
+++ source/meta.d
@@ -33,153 +33,153 @@ Window mainWindow;
 
 // justify (box)
 enum BoxJustify {
-	Left, Right, Center
+    Left, Right, Center
 }
 
 /* ******************************* MAIN WINDOW AND GUI BOXES ******************************* */
 
 // GUI window
 class Window : MainWindow {
-	// main window thread
-	private Main main;
-	
-	this(const string windowTitle, const int windowWidth, const int windowHeight, void function(MainWindow) addElements, string[] args = null) {
-		// init main
-		main = Main();
-		
-		// init main window thread
-		main.init(args);
-		
-		// call the parent constructor
-		super(windowTitle);
-		
-		// set window size
-		setSizeRequest(windowWidth, windowHeight);
-		
-		// attach onDestroy function
-		addOnDestroy(&quitApp);
-		
-		// add gui elements
-		addElements(this);
-				
-		// display the window
-		showAll();
-		
-		// run the app
-		main.run();
-	}
-	
-	// quit
-	void quitApp(Widget widget) {		
-		main.quit();
-	}
-	
-	// set Main
-	void setMainThread(ref Main m) {
-		this.main = m;
-	}
-	
-	// get Main
-	Main getMainThread() {
-		return main;
-	}
+    // main window thread
+    private Main main;
+    
+    this(in string windowTitle, in int windowWidth, in int windowHeight, void function(MainWindow) addElements, string[] args = null) {
+        // init main
+        main = Main();
+        
+        // init main window thread
+        main.init(args);
+        
+        // call the parent constructor
+        super(windowTitle);
+        
+        // set window size
+        setSizeRequest(windowWidth, windowHeight);
+        
+        // attach onDestroy function
+        addOnDestroy(&quitApp);
+        
+        // add gui elements
+        addElements(this);
+                
+        // display the window
+        showAll();
+        
+        // run the app
+        main.run();
+    }
+    
+    // quit
+    void quitApp(Widget widget) {		
+        main.quit();
+    }
+    
+    // set Main
+    void setMainThread(ref Main m) {
+        this.main = m;
+    }
+    
+    // get Main
+    Main getMainThread() {
+        return main;
+    }
 }
 
 // a box (container) containing gui elements
 class GUIBox : Box {	
-	this(const Orientation o, const int opadding, void function(Box) addElements) {
-		// calling parent's constructor
-		super(o, opadding);
-		
-		// packing the element in the window
-		addElements(this);	
-	}
+    this(in Orientation o, in int opadding, void function(Box) addElements) {
+        // calling parent's constructor
+        super(o, opadding);
+        
+        // packing the element in the window
+        addElements(this);	
+    }
 }
 
 // horizontal box
 class HBox : Box {
-	// constructor
-	this(Widget widget, BoxJustify justify, bool expand = false, bool fill = false, int padding = 0, int borderWidth = 7) {
-		// calling parent's constructor
-		super(Orientation.HORIZONTAL, 0);
-		
-		// add an element based off justify enum
-		with(BoxJustify) {
-			if(justify == Left) {
-				packStart(widget, expand, fill, padding);
-			} else if(justify == Right) {
-				packEnd(widget, expand, fill, padding);
-			} else {
-				add(widget);
-			}
-		}
-		
-		// border width
-		setBorderWidth(borderWidth);
-	}
+    // constructor
+    this(Widget widget, BoxJustify justify, bool expand = false, bool fill = false, int padding = 0, int borderWidth = 7) {
+        // calling parent's constructor
+        super(Orientation.HORIZONTAL, 0);
+        
+        // add an element based off justify enum
+        with(BoxJustify) {
+            if(justify == Left) {
+                packStart(widget, expand, fill, padding);
+            } else if(justify == Right) {
+                packEnd(widget, expand, fill, padding);
+            } else {
+                add(widget);
+            }
+        }
+        
+        // border width
+        setBorderWidth(borderWidth);
+    }
 }
 
 // label box
 class PadLabel : HBox {
-	// constructor 
-	this(BoxJustify justify, const string text = null) {
-		super(new Label(text), justify);
-	}
+    // constructor 
+    this(BoxJustify justify, in string text = null) {
+        super(new Label(text), justify);
+    }
 }
 
 class PadEntry : HBox {
-	// variables
-	private Entry entry;
-	
-	// constructor
-	this(BoxJustify justify, const string text = "") {
-		// create a new entry
-		entry = new Entry(text);
-		
-		// calling parent's constructor
-		super(entry, justify);
-	}
-	
-	// visibily
-	void setVisibility(bool state) {
-		entry.setVisibility(state);
-	}
-	
-	// character width
-	void setWidthChars(int width) {
-		entry.setWidthChars(width);
-	}
-	
-	// get data
-	string getText() {
-		return entry.getText();
-	}
-	
-	// set text
-	void setText(const string text) {
-		entry.setText(text);
-	}
+    // variables
+    private Entry entry;
+    
+    // constructor
+    this(BoxJustify justify, in string text = "") {
+        // create a new entry
+        entry = new Entry(text);
+        
+        // calling parent's constructor
+        super(entry, justify);
+    }
+    
+    // visibily
+    void setVisibility(bool state) {
+        entry.setVisibility(state);
+    }
+    
+    // character width
+    void setWidthChars(int width) {
+        entry.setWidthChars(width);
+    }
+    
+    // get data
+    string getText() {
+        return entry.getText();
+    }
+    
+    // set text
+    void setText(in string text) {
+        entry.setText(text);
+    }
 }
 
 /* ************************************* SUBMENU ITEMS ************************************* */
 
 // a submenu item that is attached to a menu header (menu header is attached to menu bar)
 class SubmenuItem : MenuItem {	
-	// constructor
-	this(const string label, void function(MenuItem) addActions) {
-		// calling parent's constructor
-		super(label);
-		
-		addActions(this);
-	}
+    // constructor
+    this(in string label, void function(MenuItem) addActions) {
+        // calling parent's constructor
+        super(label);
+        
+        addActions(this);
+    }
 }
 
 /* ************************************* DIALOG WINDOW ************************************* */
 
 // dialog interface
 interface IDialog {
-	void onAction(int, Dialog);
-	Grid getInstance();
+    void onAction(int, Dialog);
+    Grid getInstance();
 }
 
 // a dialog form that is inherited by custom dialogs 
@@ -188,78 +188,78 @@ abstract class DialogForm: Grid, IDialog {}
 
 // a dialog window that accepts a dialog form and executes it
 class DialogWindow : Dialog {
-	// constructor
-	this(Window parent, const string dialogTitle, string[] buttons, ResponseType[] responseType, DialogForm d) {
-		// calling parent constructor
-		super(dialogTitle, parent, DialogFlags.MODAL | DialogFlags.DESTROY_WITH_PARENT, buttons, responseType);
-		
-		// create new content area
-		getContentArea().add(d.getInstance);
-		getContentArea().showAll();
-				
-		// addActions
-		addOnResponse(&d.onAction);
-		
-		// run and quit
-		run();
-	}
+    // constructor
+    this(Window parent, in string dialogTitle, string[] buttons, ResponseType[] responseType, DialogForm d) {
+        // calling parent constructor
+        super(dialogTitle, parent, DialogFlags.MODAL | DialogFlags.DESTROY_WITH_PARENT, buttons, responseType);
+        
+        // create new content area
+        getContentArea().add(d.getInstance);
+        getContentArea().showAll();
+                
+        // addActions
+        addOnResponse(&d.onAction);
+        
+        // run and quit
+        run();
+    }
 }
 
 // custom dialog action handler (contains a single message and buttons + onAction, when one of the buttons is pressed)
 class CustomDialogAction : DialogForm {	
-	// variables
-	private PadLabel label;
-	
-	// custom onAction function
-	void function(int, Dialog) action;
-	
-	//constructor
-	this(string msg, void function(int, Dialog) onAction, int borderWidth = 7, int marginBottom = 7) {
-		// calling parent's constructor
-		super();
-		
-		// save function
-		action = onAction;
-		
-		// border width
-		setBorderWidth(borderWidth);
-		
-		// column 1
-		label = new PadLabel(BoxJustify.Center, msg);
-		attach(label, 1, 0, 1, 1);
-		
-		// set margin space
-		setMarginBottom(marginBottom);
-	}
-	
-	// do something on action
-	void onAction(int r, Dialog d) {
-		action(r, d);
-	}
-	
-	// get grid instance
-	Grid getInstance() {
-		return this;
-	}
+    // variables
+    private PadLabel label;
+    
+    // custom onAction function
+    void function(int, Dialog) action;
+    
+    //constructor
+    this(string msg, void function(int, Dialog) onAction, int borderWidth = 7, int marginBottom = 7) {
+        // calling parent's constructor
+        super();
+        
+        // save function
+        action = onAction;
+        
+        // border width
+        setBorderWidth(borderWidth);
+        
+        // column 1
+        label = new PadLabel(BoxJustify.Center, msg);
+        attach(label, 1, 0, 1, 1);
+        
+        // set margin space
+        setMarginBottom(marginBottom);
+    }
+    
+    // do something on action
+    void onAction(int r, Dialog d) {
+        action(r, d);
+    }
+    
+    // get grid instance
+    Grid getInstance() {
+        return this;
+    }
 }
 
 /* ************************************ OTHER FUNCTIONS ************************************ */
 
 // convert a decimal number string to float
-float strTo(T)(const string text) {
-	import std.array: replace;
-	import std.conv: to;
-	
-	return (text.replace(",", ".").to!T);
+float strTo(T)(in string text) {
+    import std.array: replace;
+    import std.conv: to;
+    
+    return (text.replace(",", ".").to!T);
 }
 
-string replaceEmptyWith(const string oldValue, const string newValue) {
-	import std.array: empty;
-	
-	if(oldValue.empty) {
-		return newValue;
-	}
-	return oldValue;
+string replaceEmptyWith(in string oldValue, in string newValue) {
+    import std.array: empty;
+    
+    if(oldValue.empty) {
+        return newValue;
+    }
+    return oldValue;
 }
 
 
